5349dfc962a847d3eb6d71c2a6af5efbf7147be1
Bilal-EZ-ZAIM <bilalzail499@gmail.com>
2024-11-01 15:14:51 +0100

    firs

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    res.json({
+        status:200,
+        message: 'Hello World from Express JS and Node JS'
+    });
+});
+
+app.listen(8080, () => {
+    console.log(`Server is running on port 8080`);
+});
+
+module.exports = app;
